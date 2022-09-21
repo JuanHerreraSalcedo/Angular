36f3c9cdc048513e27b50491d59a44e66a1844d2
JHerrera5 <jherrera@existaya.com>
2022-09-21 10:42:43 -0500

    se hace inicio del curso de fundamentos

---
+++ contador/src/app/counter/counter.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-contador',
+    templateUrl: "./counter.component.html",
+})
+
+export class CounterComponent {
+    // Titulo = 'contador';
+    // nombre: string = "Juan"
+    contador: number = 0;
+
+    handleCount( value: number){
+        // console.log('value')
+       this.contador = value++;
+    }
+
+    disCount( Value:number){
+        this.contador = Value--;
+    }
+}
