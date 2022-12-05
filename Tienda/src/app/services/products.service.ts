fddefd36920131f9fb2386d853ba9c5ea9bd1a9c
JHerrera5 <jherrera@existaya.com>
2022-12-05 17:30:45 -0500

    se ajustan actualizaciones

--- Tienda/src/app/services/products.service.ts
+++ Tienda/src/app/services/products.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { Product, createProductDTO } from './../models/product.model'
+import { Product, createProductDTO, UpdateProductDTO } from './../models/product.model'
 @Injectable({
   providedIn: 'root'
 })
@@ -23,5 +23,11 @@ export class ProductsService {
 
   create(dto: createProductDTO) {
     return this.http.post<Product>(this.apiUrl, dto);
+  }
+
+  update(id: string, dto:UpdateProductDTO) {
+    return this.http.put<Product>(`${this.apiUrl}/${id}`, dto);
+    // put deberia enviar toda la informacion del producto
+    // patch para hacer la edicion de un atributo en particular
   }
 }
